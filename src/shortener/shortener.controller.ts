c18e6b90c3ec41c3c8fef12309a2a43a5d4fad9a
nedu <chinedu@bento.africa>
2021-12-07 07:15:09 +0100

    rework controller methods to send json response

--- src/shortener/shortener.controller.ts
+++ src/shortener/shortener.controller.ts
@@ -1,18 +1,47 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { ShortenerService } from './shortener.service';
+import { EncodeBody, StatusCode } from '../types';
 
 @Controller('shortener')
 export class ShortenerController {
   constructor(private readonly shortenerService: ShortenerService) {}
   @Post('encode')
-  async encode(@Body() data: { url: string }) {
-    const { url } = data;
+  async encode(
+    @Res() res: Response,
+    @Body() data: EncodeBody,
+  ): Promise<Response> {
+    try {
+      const { url } = data;
 
-    return await this.shortenerService.encodeFromURL(url);
+      const responseData = await this.shortenerService.encodeURL(url);
+      return res.status(StatusCode.Success).send({
+        message: 'url successfully encoded',
+        data: responseData,
+      });
+    } catch (error) {
+      return res.status(StatusCode.Failure).json({
+        message: error.message || 'an error occurred while encoding url',
+      });
+    }
   }
 
   @Get('decode/:code')
-  async decode(@Param('code') code: string) {
-    return await this.shortenerService.getURLFromCode(code);
+  async decode(
+    @Res() res: Response,
+    @Param('code') code: string,
+  ): Promise<Response> {
+    try {
+      const responseData = await this.shortenerService.decodeURL(code);
+
+      return res.status(StatusCode.Success).send({
+        message: 'url has been successfully decoded',
+        data: responseData,
+      });
+    } catch (error) {
+      return res.status(StatusCode.Failure).send({
+        message: error.message || 'an error occurred while retrieving url',
+      });
+    }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,14 @@
 export type UrlData = {
+  shortUrl?: string;
   code: string;
   longUrl: string;
+};
+
+export enum StatusCode {
+  Success = 200,
+  Failure = 400,
+}
+
+export type EncodeBody = {
+  url: string;
 };
